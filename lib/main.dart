87b3e2584ae7ee3054073a994a6e908630e4d0e9
maeva <maeganelys@gmail.com>
2022-01-12 13:06:39 +0400

    after adding sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   return runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  AudioCache player = AudioCache(prefix: 'assets/');
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.white,
-            body: SafeArea(
-              child: Column(
-                children: [
-                  Expanded(
-                      child:
-                      Container(
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: Column(
+              children: [
+                Expanded(
+                    child: TextButton(
+                      onPressed:(){player.play('note1.wav');},
+                      child: Container(
                         color: Colors.red,
-                      )
-                  ),
-                  Expanded(
-                      child:
-                      Container(
+                      ),
+                    )
+                ),
+                Expanded(
+                    child: TextButton(
+                      onPressed:(){player.play('note2.wav');},
+                      child: Container(
                         color: Colors.deepOrange,
-                      )
-                  ),
-                  Expanded(
-                      child:
-                      Container(
+                      ),
+                    )
+                ),
+                Expanded(
+                    child:
+                    TextButton(
+                      onPressed:(){player.play('note3.wav');},
+                      child: Container(
                         color: Colors.yellow,
-                      )
-                  ),
-                  Expanded(
-                      child:
-                      Container(
+                      ),
+                    )
+                ),
+                Expanded(
+                    child:
+                    TextButton(
+                      onPressed:(){player.play('note4.wav');},
+                      child: Container(
                         color: Colors.green,
-                      )
-                  ),
-                  Expanded(
-                      child:
-                      Container(
+                      ),
+                    )
+                ),
+                Expanded(
+                    child:
+                    TextButton(
+                      onPressed:(){player.play('note5.wav');},
+                      child: Container(
                         color: Colors.blueGrey,
-                      )
-                  ),
-                  Expanded(
-                      child:
-                      Container(
+                      ),
+                    )
+                ),
+                Expanded(
+                    child:
+                    TextButton(
+                      onPressed:(){player.play('note6.wav');},
+                      child: Container(
                         color: Colors.blue,
-                      )
-                  ),
-                  Expanded(
-                      child:
-                      Container(
+                      ),
+                    )
+                ),
+                Expanded(
+                    child:
+                    TextButton(
+                      onPressed:(){player.play('note7.wav');},
+                      child: Container(
                         color: Colors.purple,
-                      )
-                  ),
-
-
-
-                ],
-              ),
-            )
-        ),
-      );
-    }
+                      ),
+                    )
+                ),
+              ],
+            ),
+          )
+      ),
+    );
   }
-
+}
